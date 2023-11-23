faad937a9b9e9a86af4f8e989b83e3f6297c157e
Pepe García <pepe@pepegar.com>
2023-11-23 15:48:04 +0100

    several changes
    add explanaitions
    
    and create the counter class

--- oop_recap.py
+++ oop_recap.py
@@ -16,3 +16,38 @@ print(chair1.color)
 print(chair1.number_of_legs)
 
 # %%
+
+def say_hello(first_name = "", middle = "", last_name = ""):
+    print("hello " + first_name + " " + last_name)
+
+say_hello("Alexis")
+say_hello(first_name = "Alexis", second_name = "Sanchez")
+say_hello()
+
+
+#%%
+def one():
+    name = "Aron"
+
+
+def two():
+    print(name)
+#%%
+
+class Counter:
+    def __init__(self, number_of_people = 0):
+        self.number_of_people = number_of_people
+
+    def increase(self):
+        self.number_of_people += 1
+
+
+counter1 = Counter()
+counter2 = Counter()
+
+counter1.increase()
+counter2.increase()
+
+print(counter1.number_of_people)
+print(counter2.number_of_people)
+# %%
